b591d55d2d87fa207c2ca29c9b6b3c4824db770e
zFunx <20762882+zFunx@users.noreply.github.com>
2022-10-09 21:31:21 +0530

    blank movie page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 // Components
 import MoviesMain from "./components/MoviesMain/Movie/MoviesMain";
+import MoviePage from "./components/MoviePage/MoviePage";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <nav class="navbar navbar-light bg-warning">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">Movies Task</a>
         </div>
       </nav>
       <div className="container py-5">
-        <MoviesMain />
+        <Routes>
+          <Route exact path="/" element={<MoviesMain />} />
+          <Route exact path="/movies/:id" element={<MoviePage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 

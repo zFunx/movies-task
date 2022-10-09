d5df90e0a385052cd16cb43826500b2129122374
zFunx <20762882+zFunx@users.noreply.github.com>
2022-10-09 21:08:33 +0530

    Create movie cards

--- src/components/MoviesMain/Movie/Movie.js
+++ src/components/MoviesMain/Movie/Movie.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const Movie = () => {
-  return (
-    <div>Movie</div>
-  )
+const Movie = props => {
+    return (
+        <div class="card bg-light h-100 p-cursor">
+            <img src={`https://image.tmdb.org/t/p/original/${props.poster_path}`} class="card-img-top" alt={props.title} loading="lazy" />
+            <div class="card-body">
+                <h5 class="card-title">{props.title}</h5>
+                <p class="card-text truncate-3">{props.overview}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Movie
+
+Movie.propTypes = {
+    title: PropTypes.string,
+    overview: PropTypes.string,
+    poster_path: PropTypes.string
+};
--- src/components/MoviesMain/Movie/MoviesMain.js
+++ src/components/MoviesMain/Movie/MoviesMain.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+// Components
 import Movie from './Movie'
 
+// Helpers
+import axios from 'axios'
+
+
+
 const MoviesMain = () => {
-  return (
-    <Movie />
-  )
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
+
+    function fetchMovies() {
+        axios.get('https://movie-task.vercel.app/api/popular?page=1')
+            .then(res => {
+                setMovies(res.data.data.results)
+                setLoading(false)
+            })
+            .catch(err => {
+                console.error('Something went wrong while getting the movies', err)
+                setLoading(false)
+                setError('Something went wrong while getting the movies')
+            })
+    }
+
+    if (loading) {
+        return (
+            <div class="text-center py-5">
+                <div class="spinner-border text-warning" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        )
+    }
+    else if (error) {
+        return (
+            <div class="alert alert-danger d-flex align-items-center my-5" role="alert">
+                <div>
+                   {error}
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div class="row">
+
+                {movies.map(movie => (
+                    <div class="col-12 col-md-6 col-lg-4 mb-4">
+                        <Movie key={movie.id} {...movie} />
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
 }
 
 export default MoviesMain
